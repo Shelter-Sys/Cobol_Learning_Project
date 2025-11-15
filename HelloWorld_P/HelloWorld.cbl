e59f4a996945f83cd89fb9158181c701e8a17563
Shelter-Sys <ambroggi.julien@gmail.com>
2025-11-15 18:50:11 +0100

    FirstComit

---
+++ HelloWorld_P/HelloWorld.cbl
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. 'HelloWorld'.
+       AUTHOR. Shelter-Sys .
+
+       PROCEDURE DIVISION.
+           DISPLAY 'Hello World!!' .
+           DISPLAY "j'aime le Cobol".
+           STOP RUN .
